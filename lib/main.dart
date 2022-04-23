42f1dd8677df93c04f451330c81e30363c9c327f
YEAN <yeanseyha7@gmail.com>
2022-04-23 10:20:35 +0700

    Completed Recipe Detail Cards

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'recipe.dart';
+import 'recipe_detail.dart';
 
 void main() {
   runApp(const RecipeApp());
@@ -8,7 +9,7 @@ void main() {
 class RecipeApp extends StatelessWidget {
   const RecipeApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  // This the theme or root widget of the application
   @override
   Widget build(BuildContext context) {
     final ThemeData theme = ThemeData();
@@ -28,47 +29,76 @@ class RecipeApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+// Clickable on image to each route
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    // 1
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
+      // 2
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      // 3
+      body: SafeArea(
+        // 4
+        // 4
+        child: ListView.builder(
+          // 5
+          itemCount: Recipe.samples.length,
+          // 6
+          itemBuilder: (BuildContext context, int index) {
+            // 7
+            return GestureDetector(
+              // 8
+              onTap: () {
+                // 9
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return RecipeDetail(recipe: Recipe.samples[index]);
+                    },
+                  ),
+                );
+              },
+              // 11
+              child: buildRecipeCard(Recipe.samples[index]),
+            );
+          },
         ),
-        body: SafeArea(
-          // Replace child: Container
-          child: ListView.builder(
-            itemCount: Recipe.samples.length,
-            itemBuilder: (BuildContext context, int index) {
-              // TODO: update to return Recipe card
-              return buildRecipeCard(Recipe.samples[index]);
-            },
-          ),
-        ));
+      ),
+    );
   }
 
-  // Add buildRecipeCard() here
+// Style of the Recipe card
   Widget buildRecipeCard(Recipe recipe) {
     return Card(
-      child: Column(children: <Widget>[
-        Image(image: AssetImage(recipe.imageUrl)),
-        Text(recipe.label),
-      ]),
-    );
+        elevation: 2.0,
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(children: <Widget>[
+            Image(image: AssetImage(recipe.imageUrl)),
+            const SizedBox(
+              height: 14.0,
+            ),
+            Text(
+              recipe.label,
+              style: const TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.w700,
+                fontFamily: 'Palatino',
+              ),
+            )
+          ]),
+        ));
   }
 }
